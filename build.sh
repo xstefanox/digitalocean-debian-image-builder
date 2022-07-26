e61a5d0fd18c69a62c5246dc48c24acccc6fe741
Stefano Varesi <stefano.varesi@gmail.com>
2022-07-26 20:41:46 +0200

    refactor: use a HCL file for credentials

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ INSTALL_GLUSTERFS=${INSTALL_GLUSTERFS:-false}
 INSTALL_DOCKER=${INSTALL_DOCKER:-false}
 
 packer build \
-  -var-file "${DIRNAME}/credentials.json" \
+  -var-file "${DIRNAME}/credentials.pkrvars.hcl" \
   -var "install_glusterfs=$INSTALL_GLUSTERFS" \
   -var "install_docker=$INSTALL_DOCKER" \
   debian.pkr.hcl
